a3a361a1c4b0ffc52c4af87fecb42db70669db7f
Sondre Halvorsen <sondre.halvorsen@nav.no>
2022-06-16 20:40:13 +0200

    Gradle fil som faktisk funker

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,62 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+val ktorVersion = "2.0.2"
+val kafkaVersion = "3.1.0"
+val junitJupiterVersion = "5.8.2"
+val kotlinVersion = "1.6.20"
+
 
 plugins {
-    kotlin("jvm") version "1.6.21"
-    application
+    kotlin("jvm") version "1.6.20"
 }
-
-group = "no.nav"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://packages.confluent.io/maven/")
+    maven("https://jitpack.io")
+}
+
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+    withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            events("skipped", "failed")
+        }
+    }
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-}
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
 
-tasks.test {
-    useJUnitPlatform()
-}
+    implementation("org.apache.kafka:kafka-clients:$kafkaVersion")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
 
-application {
-    mainClass.set("MainKt")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.13.3")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.3")
+
+
+    implementation("org.slf4j:slf4j-api:1.7.36")
+    implementation("ch.qos.logback:logback-classic:1.2.11")
+    implementation("net.logstash.logback:logstash-logback-encoder:7.2")
+
+
+    api("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
+    api("io.micrometer:micrometer-registry-prometheus:1.9.0")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+    testImplementation("org.awaitility:awaitility:4.2.0")
 }
